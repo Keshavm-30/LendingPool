8bc84964b6de226fed5c7393cc22f019fe901a83
Keshavm-30 <keshavmg97@gmail.com>
2024-02-10 03:04:51 +0530

    withdrawInterest

--- contracts/LendingPool.sol
+++ contracts/LendingPool.sol
@@ -13,11 +13,12 @@ contract LendingPool is Ownable{
     uint public totalInterestAccumulated;
     uint public BORROWER_INTEREST_RATE = 1000;
     uint public DEPOSITOR_INTEREST_RATE = 500;
+    uint public MIN_LIQUIDITY = 1000;
     uint totalLoanIDs;
 
     struct DepositorDetail{
         uint totalOwed;
-        uint timeOfDeposit;
+        uint lastTimeOfDeposit;
         uint totalDeposit;
     }
 
@@ -32,8 +33,14 @@ contract LendingPool is Ownable{
     mapping(uint => BorrowerDetails) public loanID;
     mapping(address=> bool) isCollateralWhitelisted;
 
+    event borrow(address borrower, uint loanID, uint amountBorrowed);
+
     error ZeroAddress();
+    error ZeroAmount();
     error AlreadyWhitelisted();
+    error  NotWhiteListed();
+    error AssetLiquidated();
+    error NotAdepositor();
 
     constructor(address _mockUSDT) Ownable(msg.sender){
         if(_mockUSDT == address(0)){
@@ -50,7 +57,65 @@ contract LendingPool is Ownable{
     }
 
     function provideLiquidity(uint _amount) external{
+        if(_amount == 0){
+            revert ZeroAmount();
 
+        }
+        DepositorDetail storage _depositordetail = depositor[msg.sender];
+        uint epoctimeDiff = block.timestamp - _depositordetail.lastTimeOfDeposit;
+        uint interestAmount = calculateInterestDepositor(_depositordetail.totalOwed,epoctimeDiff);
+        mockUSDT.transferFrom(msg.sender,address(this),_amount);
+        _depositordetail.totalOwed += _amount+interestAmount;
+        _depositordetail.lastTimeOfDeposit= block.timestamp;
+        _depositordetail.totalDeposit += _amount;
+
+    }
+
+    function borrow(uint _amount, address _collateralToken) external{
+        if(_amount==0){
+            revert ZeroAmount();
+        }
+        if(!isCollateralWhitelisted[_collateralToken]){
+            revert NotWhiteListed();
+        }
+        BorrowerDetails storage _borrowerDetail = loanID[totalLoanIDs];
+        Icollateral(_collateralToken).transferFrom(msg.sender,address(this),_amount);
+        mockUSDT.transfer(msg.sender,_amount);
+        _borrowerDetail.borrower = msg.sender;
+        _borrowerDetail.collateralToken = _collateralToken;
+        _borrowerDetail.timeOfBorrow = block.timestamp;
+        _borrowerDetail.borrowedAmount = _amount;
+        emit borrow(msg.sender,totalLoanIDs,_amount);
+        totalLoanIDs++;
+    }
+
+    function repay(uint _amount, uint _loanID) external{
+      BorrowerDetails storage _borrowerDetail = loanID[_loanID];
+      if(_borrowerDetail.borrower!= msg.sender){
+          revert NotTheCaller();
+      }
+        uint epoctimeDiff = block.timestamp - _borrowerDetail.timeOfBorrow;
+        uint interestAmount = calculateInterestBorrower(_borrowerDetail.borrowedAmount,epoctimeDiff);
+        if(interestAmount>=_borrowerDetail.borrowedAmount){
+            revert AssetLiquidated();
+        }
+      mockUSDT.transferFrom(msg.sender,address(this),_amount+interestAmount);
+      Icollateral(_borrowerDetail.collateralToken).transfer(msg.sender,_amount);
+      _borrowerDetail.borrowedAmount =0;
+      totalInterestAccumulated+ = interestAmount;
+    }
+
+
+    function withdrawInterest() external{
+        DepositorDetail storage _depositordetail = depositor[msg.sender];
+        if(_depositordetail.totalOwed==0){
+            revert NotAdepositor();
+        }
+         uint epoctimeDiff = block.timestamp - _depositordetail.lastTimeOfDeposit;
+        uint interestAmount = calculateInterestDepositor(_depositordetail.totalOwed,epoctimeDiff);
+        _depositordetail.lastTimeOfDeposit = block.timestamp;
+        totalInterestAccumulated -= interestAmount;
+        mockUSDT.transfer(msg.sender,interestAmount);
     }
 
 
